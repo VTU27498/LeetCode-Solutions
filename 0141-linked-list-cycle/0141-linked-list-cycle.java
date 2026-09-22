0ec49ec779e3c7cd3fc8afe05bd3419534cc3e5f
VTU27498 <vtu27498@veltech.edu.in>
2026-09-22 12:13:42 +0530

    Time: 0 ms (100%), Space: 46.7 MB (72.85%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Move slow by 1 and fast by 2
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true; // cycle detected
+            }
+        }
+
+        return false; // no cycle
+    }
+}
